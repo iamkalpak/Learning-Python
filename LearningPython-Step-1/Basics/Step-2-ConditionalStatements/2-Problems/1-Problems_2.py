2d33402d192ec354f81e3f71d9b4a099a8b47dbc
Kalpak <workwithkalpak@gmail.com>
2024-11-14 17:38:42 +0530

    Conditional statements and Loops

---
+++ LearningPython-Step-1/Basics/Step-2-ConditionalStatements/2-Problems/1-Problems_2.py
@@ -0,0 +1,17 @@
+def utility(s):
+    for i in range(0,len(s)):
+        if((i%2)==0):
+            print(s[i],end='')
+
+def utility(x):
+    while x >= 0:
+        print(x,end=" ")
+        x-=1
+
+#tables
+# i=1
+# for i in range(1,10):
+#     print(i*5, end=' ')
+
+# utility("DoctorPhenomenal")
+utility(78)
